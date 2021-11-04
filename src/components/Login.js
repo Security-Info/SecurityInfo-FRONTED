aedc2254172e82ce4b45b8e71cb02407a82c74c6
JamesTorres99 <69390068+JamesTorres99@users.noreply.github.com>
2021-11-04 01:51:17 -0500

    add loggin

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
 import securityInfo from '../Image/SecurityInfo.png'
 import '../styles/Login.css';
+import Axios from 'axios';
+
+
 
 function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [user, setUser] = useState(null);
+
+
+    const BASE_URL = "http://localhost:8080/securityInfo/auth/user"
+
+    const login = async credentials=> {
+        const { data } = await Axios.post(BASE_URL,credentials);
+        return data
+    }
+
+    const handlelogin = async (event) => {
+        event.preventDefault()
+        try {
+            const user = await login({
+                    username,
+                    password
+                }
+            )
+            console.log(user)
+            setUser(user)
+            setUsername('')
+            setPassword('')
+        }catch (e){
+        }
+
+    }
+
     const changePage = (event) => {
         if(event.target.name==="home"){
             window.location = "/Home";
@@ -18,24 +50,24 @@ function Login() {
                 <img id="logo0A" src={securityInfo} alt="Soldado con espada, securityInfo."/>
             </div>
             <div id="blockLogin0A">
-            <form action="/action_page.php">
+            <form action="/action_page.php" onSubmit={handlelogin}>
                 <div>
                     <div class="blockSub0A">
                         <label for="fname" class="subtitle0A">Email</label>
                     </div>
                     <div class="blockInput0A">
-                        <input type="email" class="inputs0A" id="userInput0A" name="fname" placeholder="" required/>
+                        <input type="email"  value={username} class="inputs0A" id="userInput0A" name="usuario" placeholder="" onChange={({target}) => setUsername(target.value)} required/>
                     </div>
                     <div class="blockSub0A">
                         <label for="lname" class="subtitle0A">Contraseña</label>
                     </div>
                     <div class="blockInput0A">
-                        <input type="password" class="inputs0A" id="passInput0A" name="pass" placeholder="" required/>
+                        <input type="password" value={password} class="inputs0A" id="passInput0A" name="password" placeholder="" onChange={({target}) => setPassword(target.value)} required/>
                     </div>
                 </div>
                 <div>
                     <div class="blockMainOpt0A">
-                        <button type="button" class="optLogin0A login0A" name="home" onClick={changePage}>Ingresar</button>
+                        <button type="submit" class="optLogin0A login0A" name="home"  >Ingresar</button>
                     </div>
                     <div class="blockSecOpt0A">
                         <a href="ForwardPassword" class="optLogin0A forget0A">Olvidaste tu contraseña?</a>
